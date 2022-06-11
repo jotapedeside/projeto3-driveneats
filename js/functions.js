41dd8a934488c7d349a901803f1d974be2d7cc2a
jotapedeside <jotapedeside@gmail.com>
2022-06-11 19:57:37 -0300

    hotfix(function.js): function fix

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,28 @@
 function togglePrato(prato) {
-  const pratoClicada = document.querySelector(".selected");
+  const pratoClicada = document.querySelector(".prato.selected");
   if (pratoClicada !== null) {
     pratoClicada.classList.remove("selected");
   }
-
   prato.classList.add("selected");
 }
 
 function toggleBebida(bebida) {
-  const bebidaClicada = document.querySelector(".selected");
+  const bebidaClicada = document.querySelector(".bebida.selected");
   if (bebidaClicada !== null) {
     bebidaClicada.classList.remove("selected");
   }
-
   bebida.classList.add("selected");
 }
 
 function toggleSobremesa(sobremesa) {
-  const sobremesaClicada = document.querySelector(".selected");
+  const sobremesaClicada = document.querySelector(".sobremesa.selected");
   if (sobremesaClicada !== null) {
     sobremesaClicada.classList.remove("selected");
   }
-
   sobremesa.classList.add("selected");
+}
+
+if (pratoClicada == null && bebidaClicada == null && sobremesaClicada == null) {
+  const btn = document.querySelector(".checkout");
+  classList.add(".btn-disabler");
 }
